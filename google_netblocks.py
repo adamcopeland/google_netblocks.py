fc2f91ea7ed795f52068f767a11a05d339d2f31e
Adam Copeland <adamcopeland@protonmail.com>
2016-12-19 16:51:46 -0500

    I like functions

--- google_netblocks.py
+++ google_netblocks.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 def read_ipblocklist (filename):
 
     # retreives the IP blocking inclusion list and reads it into a list
@@ -8,12 +10,65 @@ def read_ipblocklist (filename):
 
     return lines
 
-def write_ipblocklist(new_blocklist, filename):
+def write_ipblocklist(listtowrite, filename):
 
     # writes a python list of IPs to block to the ip blocking inclusions file
 
     ipblocklist = open(filename,'w')
-    for item in new_blocklist:
+
+    for item in listtowrite:
         ipblocklist.write("%s\n" % item)
 
     return
+
+def resolve_googleipranges():
+
+    # returns a list of IP ranges for outbound SMTP from Google
+    from dns.resolver import Resolver
+    sys_r = Resolver()
+
+    # query for the list of domains included in Google's SPF records
+    answers = sys_r.query('_spf.google.com', 'TXT')
+
+    # format the returned answer into a list
+    rrset = str(answers.rrset)
+    spfdomains = rrset.split(' ')
+
+    # return only entries that have "include:"
+    # and then split them on the ':' to return only the dns addresses
+    spfdomains = [ x for x in spfdomains if 'include:' in x ]
+    spfdomains = [ i.split(':', 1)[1] for i in spfdomains ]
+
+    # iterate these to get a full list of IP addresses
+
+    ipranges = list()
+
+    for domain in spfdomains:
+        answers = sys_r.query(domain, 'TXT')
+        rrset = str(answers.rrset)
+        iplist = rrset.split(' ')
+        iplist = [ x for x in iplist if 'ip4' in x ]
+        iplist = [ i.split(':', 1)[1] for i in iplist]
+        ipranges = ipranges + iplist
+
+    return ipranges
+
+
+def main():
+
+    IP_BLOCKLIST="/tmp/ip-blocking-exceptions"
+
+    # get a list of domains included in Google's SPF records
+    ipranges = resolve_googleipranges()
+    allowedips = read_ipblocklist(IP_BLOCKLIST)
+
+    # merge what's currently in the file with the new list
+    mergedlist = sorted(list (set(ipranges + allowedips)))
+
+    # write the results into the IP Blocklist Exceptions file
+    write_ipblocklist(mergedlist, IP_BLOCKLIST)
+
+
+
+if __name__ == '__main__':
+  main()
